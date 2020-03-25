b6c9ea97b60181a331ec1aeeaf22af2575465d0d
anthonymp13 <poleskian18@gmail.com>
2020-03-25 15:29:50 -0500

    Adding a not working test

--- src/test/java/com/moviegrabber/apitests/TMDBTest.java
+++ src/test/java/com/moviegrabber/apitests/TMDBTest.java
@@ -5,6 +5,10 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,7 +25,24 @@ class TMDBTest {
 
     @Test
     void getById() {
-        assertEquals("big brain", "big brain");
+        String searchTerm = "Big Brother's Big Brain";
+        String urlString = "https://api.themoviedb.org/3/search/movie?&api_key=046dfe8634eaa822f6032db544b57f14&query=" + searchTerm;
+
+        assertEquals("Big Brother's Big Brain", "Big Brother's Big Brain");
+    }
+
+    @Test
+    public void testswapiJSON() throws Exception {
+
+        String searchTerm = "Big Brother's Big Brain";
+        String urlString = "https://api.themoviedb.org/3/search/movie?&api_key=046dfe8634eaa822f6032db544b57f14&query=" + searchTerm;
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target =
+                client.target(urlString);
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+
+        assertEquals("1", response);
     }
 
 }
