9d3b1fc8466ba731c69859d265595dbd31e44fc3
Sam Miller <sdmiller1@madisoncollege.edu>
2020-03-25 16:11:45 -0500

    started working on returning json to the request

--- src/main/java/com/moviegrabber/service/MovieData.java
+++ src/main/java/com/moviegrabber/service/MovieData.java
@@ -1,17 +1,40 @@
 package com.moviegrabber.service;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moviegrabber.model.Movie;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Path("/movies")
+@Path("/movie")
 public class MovieData {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
     @GET
-    @Produces("text/plain")
-    public Response getMessage() {
-        String output = "Hello World";
+    @Produces("application/json")
+    @Path("/{param}")
+    public Response getMovie(@PathParam("param") String title) {
+        String output = "";
+
+        Movie movie = new Movie();
+        movie.setTitle(title);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            output = mapper.writeValueAsString(movie);
+        } catch (JsonProcessingException e) {
+            logger.error(e);
+        }
+
         return Response.status(200).entity(output).build();
     }
 }
