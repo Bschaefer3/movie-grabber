e2b9e9054c3d3d1be5a312994502f539ad843f84
Sam Miller <sdmiller1@madisoncollege.edu>
2020-04-08 16:58:05 -0500

    implemented the ability to search by imdb id as well as title

--- src/main/java/com/moviegrabber/service/DataAggregator.java
+++ src/main/java/com/moviegrabber/service/DataAggregator.java
@@ -1,29 +1,139 @@
 package com.moviegrabber.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moviegrabber.model.Movie;
 import com.moviegrabber.scraper.AvailabilityScraper;
 import com.omdb.OMDBDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 
 /**
- * Aggregates the movie data from omdb and google
+ * Aggregates the movie data from omdb and google into html or json
  */
 public class DataAggregator {
 
     private OMDBDao dao = new OMDBDao();
     private AvailabilityScraper availability = new AvailabilityScraper();
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
 
     /**
-     * Gets movie data by title.
+     * Gets movie json by title.
      *
-     * @param title the movie title
-     * @return the movie data
+     * @param title the title
+     * @return the movie json by title
      */
-    public Movie getMovieDataByTitle(String title) {
+    public String getMovieJsonByTitle(String title) {
 
         com.omdb.Movie omdbData = dao.getMovieByTitle(title);
 
+        String movieJson = convertMovieToJson(omdbData);
+
+        return movieJson;
+    }
+
+    /**
+     * Gets movie json by imdb id.
+     *
+     * @param imdbId the imdb id
+     * @return the movie json by imdb id
+     */
+    public String getMovieJsonByImdbId(String imdbId) {
+
+        com.omdb.Movie omdbData = dao.getMovieById(imdbId);
+
+        String movieJson = convertMovieToJson(omdbData);
+
+        return movieJson;
+    }
+
+    /**
+     * Gets movie html by title.
+     *
+     * @param title the title
+     * @return the movie html by title
+     */
+    public String getMovieHtmlByTitle(String title) {
+        com.omdb.Movie omdbData = dao.getMovieByTitle(title);
+
+        String movieHtml = convertMovieToHtml(omdbData);
+
+        return movieHtml;
+    }
+
+    /**
+     * Gets movie html by imdb id.
+     *
+     * @param imdbId the imdb id
+     * @return the movie html by imdb id
+     */
+    public String getMovieHtmlByImdbId(String imdbId) {
+        com.omdb.Movie omdbData = dao.getMovieById(imdbId);
+
+        String movieHtml = convertMovieToHtml(omdbData);
+
+        return movieHtml;
+    }
+
+    /**
+     * Convert movie to json string.
+     *
+     * @param omdbData the omdb data
+     * @return the json string
+     */
+    public String convertMovieToJson(com.omdb.Movie omdbData) {
+        String movieJson = "";
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Movie movie = convertOmdbDataToMovie(omdbData);
+
+        try {
+            movieJson = mapper.writeValueAsString(movie);
+        } catch (JsonProcessingException e) {
+            logger.error(e);
+        }
+
+        return movieJson;
+    }
+
+    /**
+     * Convert movie to html string.
+     *
+     * @param omdbData the omdb data
+     * @return the html string
+     */
+    public String convertMovieToHtml(com.omdb.Movie omdbData) {
+        Movie movie = convertOmdbDataToMovie(omdbData);
+
+        String movieHtml = "<ul>";
+
+        movieHtml += "<li>Title: " + movie.getTitle() + "</li>";
+        movieHtml += "<li>Year: " + movie.getYear() + "</li>";
+        movieHtml += "<li>IMDB ID: " + movie.getImdbID() + "</li>";
+        movieHtml += "<li>Availability:</li>";
+        movieHtml += "<ul>";
+
+        for (Map.Entry<String, String> entry : movie.getAvailability().entrySet()) {
+            movieHtml += "<li>" + entry.getKey() + ": " + entry.getValue() + "</li>";
+        }
+
+        movieHtml += "</ul></ul>";
+
+        return movieHtml;
+    }
+
+
+    /**
+     * Convert omdb data to movie movie.
+     *
+     * @param omdbData the omdb data
+     * @return the movie
+     */
+    public Movie convertOmdbDataToMovie(com.omdb.Movie omdbData) {
         Movie movie = new Movie();
 
         movie.setTitle(omdbData.getTitle());
--- src/main/java/com/moviegrabber/service/MovieData.java
+++ src/main/java/com/moviegrabber/service/MovieData.java
@@ -1,8 +1,5 @@
 package com.moviegrabber.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.moviegrabber.model.Movie;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,7 +8,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.util.Map;
 
 /**
  * Processes api requests to get movie data
@@ -30,19 +26,9 @@ public class MovieData {
      */
     @GET
     @Produces("application/json")
-    @Path("/{param}")
-    public Response getMovie(@PathParam("param") String title) {
-        String output = "";
-
-        Movie movie = movieGrabber.getMovieDataByTitle(title);
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            output = mapper.writeValueAsString(movie);
-        } catch (JsonProcessingException e) {
-            logger.error(e);
-        }
+    @Path("/title={param}")
+    public Response getMovieByTitle(@PathParam("param") String title) {
+        String output =  movieGrabber.getMovieJsonByTitle(title);
 
         return Response.status(200).entity(output).build();
     }
@@ -55,23 +41,41 @@ public class MovieData {
      */
     @GET
     @Produces("text/html")
-    @Path("/{param}")
-    public Response getMovieHTML(@PathParam("param") String title) {
-        String output = "<ul>";
+    @Path("/title={param}")
+    public Response getMovieHTMLByTitle(@PathParam("param") String title) {
+        String output = movieGrabber.getMovieHtmlByTitle(title);
 
-        Movie movie = movieGrabber.getMovieDataByTitle(title);
+        return Response.status(200).entity(output).build();
+    }
 
-        output += "<li>Title: " + movie.getTitle() + "</li>";
-        output += "<li>Year: " + movie.getYear() + "</li>";
-        output += "<li>IMDB ID: " + movie.getImdbID() + "</li>";
-        output += "<li>Availability:</li>";
-        output += "<ul>";
+    /**
+     * Gets movie data in a json format.
+     *
+     * @param imdbId the movie id
+     * @return the movie data
+     */
+    @GET
+    @Produces("application/json")
+    @Path("/imdbid={param}")
+    public Response getMovieJsonByImdbId(@PathParam("param") String imdbId) {
 
-        for (Map.Entry<String, String> entry : movie.getAvailability().entrySet()) {
-            output += "<li>" + entry.getKey() + ": " + entry.getValue() + "</li>";
-        }
+        String output = movieGrabber.getMovieJsonByImdbId(imdbId);
 
-        output += "</ul></ul>";
+        return Response.status(200).entity(output).build();
+    }
+
+
+    /**
+     * Gets movie data in an html return format.
+     *
+     * @param imdbId the imdb ID of the movie
+     * @return the movie data
+     */
+    @GET
+    @Produces("text/html")
+    @Path("/imdbid={param}")
+    public Response getMovieHTMLByImdbId(@PathParam("param") String imdbId) {
+        String output = movieGrabber.getMovieHtmlByImdbId(imdbId);
 
         return Response.status(200).entity(output).build();
     }
