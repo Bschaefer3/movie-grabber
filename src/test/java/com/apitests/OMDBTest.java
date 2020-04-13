a43650b2cb27cd4ccc813f46ec3090b8708b4baa
anthonymp13 <poleskian18@gmail.com>
2020-04-13 16:26:23 -0500

    Added passing test for getting movies by imdb id

--- src/test/java/com/apitests/OMDBTest.java
+++ src/test/java/com/apitests/OMDBTest.java
@@ -2,15 +2,38 @@ package com.apitests;
 
 import com.omdb.Movie;
 import com.omdb.OMDBDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OMDBTest {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void getMovieByIdTest() {
+        OMDBDao omdbDao = new OMDBDao();
+
+        Movie movie = omdbDao.getMovieById("tt1190080");
+
+        assertEquals("2012", movie.getTitle());
+    }
+
     @Test
     public void testOMDBJSON() throws Exception {
         Movie movie = new OMDBDao().getMovieByTitle("The Martian");
 
         assertEquals("The Martian", movie.getTitle());
     }
+
+
 }
